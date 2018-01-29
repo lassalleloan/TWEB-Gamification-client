7b27de60cc9f412a561562662e59b4fccf8c6666
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2018-01-29 12:03:04 +0100

    Description add

--- docs/app/modules/home/homeCtrl.js
+++ docs/app/modules/home/homeCtrl.js
@@ -24,7 +24,7 @@
 		const vm = this;
 
 		vm.title = 'Hello, gamification-client!';
-		vm.summary = 'This is a template for a simple home screen website. Use it as a starting point to create something more unique.';
+		vm.summary = 'This is a client application for Gamification. This will display the user\'s scores.';
 		vm.authors = [
 			{
 				name: 'Galahad',
@@ -39,14 +39,7 @@
 			{
 				title: 'Description', 
 				/*jshint multistr: true */
-				text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper pulvinar ante in tempus. Etiam ultrices ultricies euismod. \
-						Fusce congue sapien lacus, non sodales sem ullamcorper eu. Vivamus rhoncus ipsum id sem pulvinar, et ullamcorper enim iaculis. \
-						Vivamus vitae arcu non tortor volutpat lacinia ac ut dui. Etiam id sodales lectus, ac eleifend nisi. \
-						Vivamus eget consectetur sem. Ut viverra nisl quis ex euismod, nec posuere lorem semper. Duis a porttitor mi, sit amet lacinia nisi. \
-						In ornare massa vel magna maximus, sed cursus diam interdum. Nunc in risus vel mauris dictum pulvinar. \
-						Morbi iaculis, nibh pretium gravida malesuada, sem nibh congue mauris, eget commodo augue odio at ex. \
-						Duis purus tortor, porttitor a interdum non, suscipit a eros. Etiam odio quam, mollis in ante sed, vestibulum fringilla nibh. \
-						Suspendisse ut placerat risus, non sollicitudin velit.',
+				text: 'With the addition of third-party application, it is easy to allow users to increase their score. For example, Password Meter application allows you to obtain additional points when you change your password.',
 			},
 		];
 		
